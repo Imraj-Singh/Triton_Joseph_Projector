b0ed6b9bd5221140b1211ab9da978c32c116b880
Imraj-Singh <72553490+Imraj-Singh@users.noreply.github.com>
2025-05-01 13:46:27 +0000

    timing scripts

---
+++ TIMING_parallel_proj.py
@@ -0,0 +1,77 @@
+import array_api_compat.torch as xp
+import torch
+import parallelproj
+from array_api_compat import to_device
+import array_api_compat.numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+##### CHECK if the test data was created glob slices_*_lors_*.npy
+
+from glob import glob
+all_test_data = glob("slices_*_lors_*.npy")
+if len(all_test_data) == 0:
+    print("No test data found. Please run the test_data.py script to generate it.")
+    exit()
+
+# extract slices and lors from the filename
+slices = []
+lors = []
+for test_data in all_test_data:
+    parts = test_data.split("_")
+    slices.append(int(parts[1]))
+    lors.append(int(parts[3].split(".")[0]))
+    print(f"Found test data: {test_data} with slices: {slices[-1]} and lors: {lors[-1]}")
+
+    dict_data = np.load(test_data, allow_pickle=True).item()
+    xstart = xp.asarray(dict_data['xstart']).cuda()
+    xend = xp.asarray(dict_data['xend']).cuda()
+    img_origin = xp.asarray(dict_data['img_origin']).cuda()
+    voxel_size = xp.asarray(dict_data['voxel_size']).cuda()
+    img_shape = dict_data['img_shape']
+    x_gt = xp.asarray(dict_data['x']).cuda()
+    meas = xp.asarray(dict_data['meas']).cuda() + 100
+
+    fwd = lambda x: parallelproj.joseph3d_fwd(xstart, xend, x, img_origin, voxel_size)
+
+    back = lambda y: parallelproj.joseph3d_back(xstart, xend, img_shape, img_origin, voxel_size, y)
+
+    tmp = fwd(x_gt)
+    torch.cuda.synchronize(x_gt.device)
+    start = time.time()
+    for i in range(10):
+        tmp = fwd(x_gt)
+        torch.cuda.synchronize(x_gt.device)
+    end_t = time.time()
+    forward_timing = (end_t - start) / 10
+
+    sens_img = back(xp.ones_like(meas))
+    x = xp.ones_like(sens_img)
+    torch.cuda.synchronize(x_gt.device)
+
+    start = time.time()
+    for i in range(10):
+        tmp = back(meas)
+        torch.cuda.synchronize(x_gt.device)
+    end_t = time.time()
+    backward_timing = (end_t - start) / 10
+
+    start = time.time()
+    for i in range(10):
+        em_precond = x/sens_img
+        ratio = meas/(fwd(x)+100.)
+        torch.cuda.synchronize(x_gt.device)
+        bp_ratio = back(ratio)
+        torch.cuda.synchronize(x_gt.device)
+        x = em_precond*bp_ratio
+    end_t = time.time()
+    mlem_timing = (end_t - start) / 10
+
+    number_of_lors = xstart.shape[0]*xstart.shape[1]*xstart.shape[2]
+    number_of_voxels = x.shape[0]*x.shape[1]*x.shape[2]
+
+    # save the timings to a file
+    with open("TIMING_parallel_proj.txt", "a") as f:
+        f.write(f"{number_of_lors} {number_of_voxels} {forward_timing} {backward_timing} {mlem_timing}\n")
+        print(f"Saved timings for slices: {slices[-1]} and lors: {lors[-1]} to timings.txt")
---
+++ get_test_data.py
@@ -0,0 +1,89 @@
+import array_api_compat.cupy as xp
+
+import parallelproj
+from array_api_compat import to_device
+import array_api_compat.numpy as np
+import matplotlib.pyplot as plt
+
+if "numpy" in xp.__name__:
+    dev = "cpu"
+elif "cupy" in xp.__name__:
+    dev = xp.cuda.Device(0)
+elif "torch" in xp.__name__:
+    dev = "cuda"
+
+# things we keep constant, radius, num_sides, 
+
+sl_phantom = np.swapaxes(np.load('3D_shepp_logan.npy'), 0, 1)
+print(f" The size of the Shepp Logan phantom is {sl_phantom.shape}")
+
+image_xy = sl_phantom.shape[0]
+
+list_slices = [4, 8, 16, 32, 64]
+list_lor_endpoints_per_side = [20, 25, 30, 35, 40]
+voxels_sizes = (.75, 3.0, .75)
+num_lors = []
+num_voxels = []
+
+for num_slices in list_slices:
+    for num_lor_endpoints_per_side in list_lor_endpoints_per_side:
+        
+        num_rings = num_slices//2
+        max_ring_position = (voxels_sizes[1] * num_rings) - (voxels_sizes[1] / 2)
+        scanner = parallelproj.RegularPolygonPETScannerGeometry(
+            xp,
+            dev,
+            radius=210.0,
+            num_sides=12,
+            num_lor_endpoints_per_side=num_lor_endpoints_per_side,
+            lor_spacing=108/num_lor_endpoints_per_side,
+            ring_positions=xp.linspace(-max_ring_position, max_ring_position, num_rings),
+            symmetry_axis=1, 
+        )
+
+        lor_desc = parallelproj.RegularPolygonPETLORDescriptor(
+            scanner,
+            radial_trim=1,
+            max_ring_difference=list_slices[0],
+            sinogram_order=parallelproj.SinogramSpatialAxisOrder.RVP,
+        )
+
+        proj = parallelproj.RegularPolygonPETProjector(
+            lor_desc, img_shape=(image_xy, num_slices, image_xy), voxel_size=voxels_sizes,
+        )
+        """ plt.close("all")
+        fig = plt.figure(figsize=(8, 8))
+        ax1 = fig.add_subplot(111, projection="3d")
+        proj.show_geometry(ax1)
+        fig.tight_layout()
+        fig.show() """
+        xstart, xend = proj._lor_descriptor.get_lor_coordinates(views=proj._views)
+        print(f" Number of LORs is {xstart.shape[0]*xstart.shape[1]*xstart.shape[2]}")
+        print(f" Number of Voxels is {image_xy*image_xy*num_slices}")
+        num_lors.append(xstart.shape[0]*xstart.shape[1]*xstart.shape[2])
+        num_voxels.append(image_xy*image_xy*num_slices)
+
+        if num_slices == sl_phantom.shape[1]:
+            x = xp.array(sl_phantom)
+        else:
+            x = xp.array(sl_phantom[:, (sl_phantom.shape[1]-num_slices)//2:-(sl_phantom.shape[1]-num_slices)//2])
+
+        x_fwd = parallelproj.joseph3d_fwd(
+                        xstart, xend, x, proj._img_origin, proj._voxel_size
+                    )
+                    
+        x_back = parallelproj.joseph3d_back(
+                        xstart, xend, proj._img_shape, proj._img_origin, proj._voxel_size, x_fwd,
+                    )
+
+        dict_data = {}
+
+        dict_data['xstart'] = xstart.get()
+        dict_data['xend'] = xend.get()
+        dict_data['img_origin'] = proj._img_origin.get()
+        dict_data['voxel_size'] = proj._voxel_size.get()
+        dict_data['img_shape'] = proj._img_shape
+        dict_data['x'] = x.get()
+        dict_data['meas'] = x_fwd.get()
+
+        np.save(f"slices_{num_slices}_lors_{num_lor_endpoints_per_side}.npy", dict_data)
