1daf34c5f97829036cb58b119dc2f917e462481b
Imraj-Singh <72553490+Imraj-Singh@users.noreply.github.com>
2025-05-01 14:43:13 +0000

    adding lors and slices to txt of pp timing

--- TIMING_parallel_proj.py
+++ TIMING_parallel_proj.py
@@ -78,5 +78,5 @@ for test_data in all_test_data:
 
     # save the timings to a file
     with open("TIMING_parallel_proj.txt", "a") as f:
-        f.write(f"{number_of_lors} {number_of_voxels} {forward_timing} {backward_timing} {mlem_timing}\n")
+        f.write(f"{lors[-1]} {slices[-1]} {number_of_lors} {number_of_voxels} {forward_timing} {backward_timing} {mlem_timing}\n")
         print(f"Saved timings for slices: {slices[-1]} and lors: {lors[-1]} to timings.txt")
